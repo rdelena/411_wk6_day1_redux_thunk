2d87d4b1c18e6bb05d52d780f6332c1fe8735060
rdelena <rodnydelena@gmail.com>
2023-06-21 23:59:32 -0500

    Updates

---
+++ precache-manifest.d306f77da8218899303c10e94a633642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3535a158d7b5aa29012e9e91b7d7f9f8",
+    "url": "/411_wk6_day1_redux_thunk/index.html"
+  },
+  {
+    "revision": "ed8ec1f4acc76a7848ce",
+    "url": "/411_wk6_day1_redux_thunk/static/css/main.71035251.chunk.css"
+  },
+  {
+    "revision": "3a6716045b1cac61a386",
+    "url": "/411_wk6_day1_redux_thunk/static/js/2.fb6a2ef1.chunk.js"
+  },
+  {
+    "revision": "ed8ec1f4acc76a7848ce",
+    "url": "/411_wk6_day1_redux_thunk/static/js/main.3d8d5702.chunk.js"
+  },
+  {
+    "revision": "fbf4cc4069f99ef900df",
+    "url": "/411_wk6_day1_redux_thunk/static/js/runtime~main.b32a8eb5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/411_wk6_day1_redux_thunk/precache-manifest.d306f77da8218899303c10e94a633642.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/411_wk6_day1_redux_thunk/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
